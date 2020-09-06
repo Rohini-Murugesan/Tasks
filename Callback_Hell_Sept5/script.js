2163c6b16146427c77455d22f1a211e2c176d9a0
Rohini <rohini.m@tataelxsi.co.in>
2020-09-06 08:36:30 +0530

    Callback Hell

---
+++ Callback_Hell_Sept5/script.js
@@ -0,0 +1,34 @@
+
+var t = setTimeout(function(){
+    document.getElementById("Countdown").innerHTML = "10"
+    setTimeout(function(){
+        document.getElementById("Countdown").innerHTML = "9"
+        setTimeout(function(){
+            document.getElementById("Countdown").innerHTML = "8"
+            setTimeout(function(){
+                document.getElementById("Countdown").innerHTML = "7"
+                setTimeout(function(){
+                    document.getElementById("Countdown").innerHTML = "6"
+                    setTimeout(function(){
+                        document.getElementById("Countdown").innerHTML = "5"
+                        setTimeout(function(){
+                            document.getElementById("Countdown").innerHTML = "4"
+                            setTimeout(function(){
+                                document.getElementById("Countdown").innerHTML = "3"
+                                setTimeout(function(){
+                                    document.getElementById("Countdown").innerHTML = "2"
+                                    setTimeout(function(){
+                                        document.getElementById("Countdown").innerHTML = "1"
+                                        setTimeout(function(){
+                                            document.getElementById("Countdown").innerHTML = "HAPPY NEW YEAR!!!!!!"
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
